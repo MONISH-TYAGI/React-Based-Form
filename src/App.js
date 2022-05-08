4f2b7008920c5d9a4d829fb124f7717dc0873543
Monish <monishtyagi5770@gmail.com>
2022-05-08 19:44:53 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      description: "",
+      gender: "",
+      acceptTerms: false,
+    };
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(
+      "User name: " +
+        this.state.userName +
+        "\n Description: " +
+        this.state.description +
+        "\n Gender: " +
+        this.state.gender +
+        "\n CheckBox: " +
+        this.state.acceptTerms 
+    );
+  };
+  handleChange=(event)=>{
+    this.setState({
+      [event.target.name]:event.target.type==='checkbox'?event.target.checked:event.target.value,
+    });
+  }
+  // handleUsernameChange = (event) => {
+  //   this.setState({ userName: event.target.value });
+  // };
+  // handleDescriptionChange = (event) => {
+  //   this.setState({ description: event.target.value });
+  // };
+  // handleGenderChange = (event) => {
+  //   this.setState({ gender: event.target.value });
+  // };
+  
+  // handleAcceptTermsChange = (event) => {
+  //   this.setState({ acceptTerms: event.target.checked });
+  // };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text" name="userName"
+            value={this.state.userName}
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <div>
+          <label>Gender:</label>
+          <select  name="gender" value={this.state.gender} onChange={this.handleChange}>
+            <option>Choose</option>
+            <option>Men</option>
+            <option>Women</option>
+          </select>
+        </div>
+        <div>
+          <label>Description:</label>
+          <textarea name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          ></textarea>
+        </div>
+        <div>
+          <label>Accept Terms</label>
+          <input name="acceptTerms" type="checkbox" checked={this.state.acceptTerms} onChange={this.handleChange}></input>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default App;
